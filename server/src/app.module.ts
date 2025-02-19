fe70f30ab1887475c0ca07748de2f8df3dbc0ed4
tripm1405 <tri.pm.1405@gmail.com>
2025-02-20 04:24:06 +0700

    feat: server - init connect mssql with typeorm

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: 'localhost',
+      port: 1433,
+      username: 'sa',
+      database: 'KTraffic',
+      entities: [],
+      extra: {
+        trustServerCertificate: true,
+      },
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
